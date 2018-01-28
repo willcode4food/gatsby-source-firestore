9774b240976ad8e76c7baf755beec34c4c5a8806
Marc Arbesman <marc@weedrater.com>
2018-01-28 08:45:59 -0500

    refactoring...still need a way to test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin")
 const crypto = require("crypto")
 
+
 exports.sourceNodes = async (
   { boundActionCreators },
   { credential, databaseURL, types, quiet = false },
@@ -16,50 +17,43 @@ exports.sourceNodes = async (
   const db = admin.firestore()
 
   const start = Date.now()
-
-  types.forEach(
-    ({ query = ref => ref, map = node => node, collection, path }) => {
-      try {
-        const querySnapshot = await query(db.collection(collection).get())
-      }
-      catch(error) {
-        throw new Error(`Database Error: ${error}`)
-      }
-      
+  for (let t of types){
+    const { query = async ref => ref, map = node => node, collection } = t
+    
+    try {
+      const querySnapshot = await query(db.collection(collection)).get()
       if(!querySnapshot.empty) {
         console.log(
             `\n[Firestore Source] Data for ${collection} loaded in`,
             Date.now() - start,
             "ms"
+        )
+        const docs = querySnapshot.docs()
+
+        Object.keys(val).forEach(key => {
+          const node = map(Object.assign({}, val[key]))
+          const contentDigest = crypto
+            .createHash(`md5`)
+            .update(JSON.stringify(node))
+            .digest(`hex`)
+
+          createNode(
+            Object.assign(node, {
+              id: key,
+              parent: "root",
+              children: [],
+              internal: {
+                type: type,
+                contentDigest: contentDigest
+              }
+            })
           )
+        })
+        done()
       }
-      const docs = querySnapshot.docs()
-      for(val of docs){
-            Object.keys(val).forEach(key => {
-            const node = map(Object.assign({}, val[key]))
-
-            const contentDigest = crypto
-              .createHash(`md5`)
-              .update(JSON.stringify(node))
-              .digest(`hex`)
-
-            createNode(
-              Object.assign(node, {
-                id: key,
-                parent: "root",
-                children: [],
-                internal: {
-                  collection: collection,
-                  contentDigest: contentDigest
-                }
-              })
-            )
-          })
-          done()
-      }
-    },
-    error => {
+    }
+    catch(error) {
       throw new Error(error)
     }
-  )
+  }
 }
